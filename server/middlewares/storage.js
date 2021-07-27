ffd0fd63cb03a03670946b341a1b9b4fb639385d
Georgi Stoilkov <gogost6@gmail.com>
2021-07-27 11:13:46 +0300

    Minor fix!

--- server/middlewares/storage.js
+++ server/middlewares/storage.js
@@ -1,9 +1,9 @@
-const trip = require('../services/trip');
+const auto = require('../services/auto');
 const user = require('../services/user');
 
 async function init() {
     return (req, res, next) => {
-        const storage = Object.assign({}, trip);
+        const storage = Object.assign({}, auto);
         const userData = Object.assign({}, user);
         req.storage = storage;
         req.user = userData;
