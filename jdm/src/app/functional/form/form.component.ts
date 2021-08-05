d09112465405f7165edcf41c5ff1c12d6a72b3b8
Georgi Stoilkov <gogost6@gmail.com>
2021-08-05 11:54:04 +0300

    home page form for search update

--- jdm/src/app/functional/form/form.component.ts
+++ jdm/src/app/functional/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import * as data from '../../../assets/data.json';
 import * as cars from '../../../assets/cars.json';
@@ -8,6 +9,9 @@ import * as countries from '../../../assets/countries.json';
 import { IData } from '../../shared/interfaces/data';
 import { ICars } from 'src/app/shared/interfaces/cars';
 import { ICountries } from 'src/app/shared/interfaces/countries';
+import { SearchService } from '../../services/search.service'
+import { SharedService } from '../../services/shared.service'
+import { ICar } from 'src/app/shared/interfaces/car';
 
 @Component({
   selector: 'app-form',
@@ -16,18 +20,26 @@ import { ICountries } from 'src/app/shared/interfaces/countries';
 })
 
 
-export class FormComponent{
+export class FormComponent {
   jsonData: IData = data;
   cars: ICars = cars;
   countries: ICountries = countries;
   carMake: string = "";
   city: string = "";
+  searchCars: any;
 
-  constructor() {
-    
-  }
+  constructor(
+    private searchService: SearchService,
+    private router: Router,
+    private sharedService: SharedService
+    ) { }
 
   searchHandler(form: NgForm): void {
-    console.log(form); 
+    if (form.invalid) { throw new Error('Invalid form!'); }
+    this.searchService.search(form.value).subscribe(carsArr => {
+      this.searchCars = carsArr;
+      this.sharedService.setData(this.searchCars);
+    });
+    this.router.navigate(['/search-car']);
   }
 }
