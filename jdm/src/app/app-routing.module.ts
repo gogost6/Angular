de311a935c8afb04efa37e3545aa74889d318223
Georgi Stoilkov <gogost6@gmail.com>
2021-08-05 12:45:44 +0300

    update

--- jdm/src/app/app-routing.module.ts
+++ jdm/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AboutComponent } from './core/about/about.component';
 import { FormComponent } from './functional/form/form.component';
 import { PostCarComponent } from './functional/post-car/post-car.component';
