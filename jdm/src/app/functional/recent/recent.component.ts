2f8de3c985f6175dc06bc246456a6297d32ac746
Georgi Stoilkov <gogost6@gmail.com>
2021-08-05 11:52:46 +0300

    recent cars component update

--- jdm/src/app/functional/recent/recent.component.ts
+++ jdm/src/app/functional/recent/recent.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SearchService } from 'src/app/services/search.service';
+import { ICar } from 'src/app/shared/interfaces/car';
 
 @Component({
   selector: 'app-recent',
   templateUrl: './recent.component.html',
   styleUrls: ['./recent.component.sass']
 })
-export class RecentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class RecentComponent {
+  recentCars: any;
+  
+  constructor(private searchService: SearchService, private router: Router) { 
+    this.searchService.recent().subscribe(
+      car => this.recentCars = car
+    );;
+    console.log(this.recentCars)
   }
 
+  showDetails(carId: any) {
+    try {
+      const url: any = `/details/${carId}`;
+      this.router.navigate(url)
+    } catch(err) {
+      console.log(err);
+      this.router.navigate(['/home']);
+    }
+  }
 }
