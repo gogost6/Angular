d84679aac0f042f0f56ce8725300db7114f6872f
Georgi Stoilkov <gogost6@gmail.com>
2021-08-09 14:00:30 +0300

    profile update

--- jdm/src/app/auth/profile/profile.component.ts
+++ jdm/src/app/auth/profile/profile.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../../services/user.service';
+import { SearchService } from '../../services/search.service';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.sass']
 })
 export class ProfileComponent implements OnInit {
+  cars!: any;
 
-  constructor() { }
+  constructor(private searchService: SearchService, private userService: UserService) {
+    searchService.created(JSON.parse(userService.userData).created).subscribe(carsArr => this.cars = carsArr);
+   }
 
   ngOnInit(): void {
   }
