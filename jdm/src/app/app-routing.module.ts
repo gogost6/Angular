2bb755708279bd12581bc7103f596541dc3d9ddf
Georgi Stoilkov <gogost6@gmail.com>
2021-08-12 15:31:50 +0300

    update

--- jdm/src/app/app-routing.module.ts
+++ jdm/src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutComponent } from './core/about/about.component';
-import { DetailsComponent } from './functional/details/details.component';
 import { FormComponent } from './functional/form/form.component';
-import { PostCarComponent } from './functional/post-car/post-car.component';
-import { SearchComponent } from './functional/search/search.component';
 
 const routes: Routes = [
   {
@@ -16,31 +13,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: FormComponent
-  },
-  {
-    path: 'search',
-    component: SearchComponent
-  },
+},
   {
     path: 'about',
     component: AboutComponent
-  },
-  {
-    path: 'post-car',
-    component: PostCarComponent
-  },
-  {
-    path: 'search-car',
-    component: SearchComponent
-  },
-  {
-    path: 'details/:id',
-    component: DetailsComponent
-  },
-  {
-    path: 'auth/logout',
-    pathMatch: 'full',
-    redirectTo: '/home'
   },
 ];
 
