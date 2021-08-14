dac92bfeaa193bdbdde6fabe567c390a1eebcbde
Georgi Stoilkov <gogost6@gmail.com>
2021-08-14 17:23:57 +0300

    module updates

--- jdm/src/app/app.module.ts
+++ jdm/src/app/app.module.ts
@@ -2,22 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
+
+import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-import { FunctionalModule } from './functional/functional.module';
-import { LoaderComponent } from './shared/loader/loader.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    FunctionalModule,
     BrowserModule,
     CoreModule,
-    AuthModule,
     AppRoutingModule,
     HttpClientModule
   ],
--- jdm/src/app/core/core.module.ts
+++ jdm/src/app/core/core.module.ts
@@ -5,6 +5,9 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
+import { FunctionalModule } from '../functional/functional.module';
+import { AuthModule } from '../auth/auth.module';
+
 
 
 @NgModule({
@@ -14,7 +17,9 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FunctionalModule,
+    AuthModule
   ],
   exports: [
     HeaderComponent
