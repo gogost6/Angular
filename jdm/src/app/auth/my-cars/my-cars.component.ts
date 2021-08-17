fece32174880d63e0782da716d44ba4e767a4445
Georgi Stoilkov <gogost6@gmail.com>
2021-08-17 15:59:59 +0300

    async pipe added!

--- jdm/src/app/auth/my-cars/my-cars.component.ts
+++ jdm/src/app/auth/my-cars/my-cars.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { UserService } from '../../services/user.service';
 import { SearchService } from '../../services/search.service';
+import { Observable } from 'rxjs';
+import { ICar } from 'src/app/shared/interfaces/car';
 
 @Component({
   selector: 'app-my-cars',
@@ -9,9 +11,9 @@ import { SearchService } from '../../services/search.service';
   styleUrls: ['./my-cars.component.sass']
 })
 export class MyCarsComponent {
-  cars: any = [];
+  cars$: Observable<ICar[]>;
 
   constructor(private searchService: SearchService, private userService: UserService) {
-    searchService.created(JSON.parse(userService.userData).createdAutos).subscribe(carsArr => this.cars = carsArr);
+    this.cars$ = searchService.created(JSON.parse(userService.userData).createdAutos);
   }
 }
