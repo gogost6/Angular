c9899af9b3eab9f1ecea01f8413b8b01f81ad3e1
Georgi Stoilkov <gogost6@gmail.com>
2021-08-25 13:42:30 +0300

    update

--- server/services/auto.js
+++ server/services/auto.js
@@ -1,81 +1,94 @@
-const Auto = require('../models/Auto');
-const User = require('../models/User');
-const mongoose = require('mongoose');
+const Auto = require("../models/Auto");
+const User = require("../models/User");
+const mongoose = require("mongoose");
 
 async function getAll() {
-    const autos = Auto.find({}).lean();
-    return autos;
+  const autos = Auto.find({}).lean();
+  return autos;
 }
 
 async function getRecent() {
-    const autos = Auto.find({}).sort({ date: -1 }).limit(3).lean();
-    return autos;
+  const autos = Auto.find({}).sort({ date: -1 }).limit(3).lean();
+  return autos;
 }
 
 async function getCarsByCriteria(data) {
-    let obj = {};
-    let criteria = Object.entries(data).filter(x => x[1] !== '').forEach(x => {
-        if (x[0] == 'minPrice' || x[0] == 'maxPrice') {
-            obj.price = {};
-        }
-        if (x[0] == 'dateMade') {
-            obj[x[0]] = { $gte: new Date(x[1].slice(6)) };
-        } else if (x[0] == 'minPrice') {
-            Object.assign(obj.price, { $gte: Number(x[1]) });
-        } else if (x[0] == 'maxPrice') {
-            Object.assign(obj.price, { $lt: Number(x[1]) });
-        } else {
-            obj[x[0]] = x[1];
-        }
+  let obj = {};
+  let criteria = Object.entries(data)
+    .filter((x) => x[1] !== "")
+    .forEach((x) => {
+      if (x[0] == "minPrice" || x[0] == "maxPrice") {
+        obj.price = {};
+      }
+      if (x[0] == "dateMade") {
+        obj[x[0]] = { $gte: new Date(x[1].slice(6)) };
+      } else if (x[0] == "minPrice") {
+        Object.assign(obj.price, { $gte: Number(x[1]) });
+      } else if (x[0] == "maxPrice") {
+        Object.assign(obj.price, { $lt: Number(x[1]) });
+      } else {
+        obj[x[0]] = x[1];
+      }
     });
-    const autos = Auto.find(obj).lean();
-    return autos;
+  const autos = Auto.find(obj).lean();
+  return autos;
 }
 
 async function create(auto, email) {
-    let user = await User.findOne({ email });
-    Object.assign(auto, { owner: user });
-    const record = new Auto(auto);
-    await record.save();
-    user.createdAutos.push(record);
-    await user.save();
+  let user = await User.findOne({ email });
+  Object.assign(auto, { owner: user });
+  const record = new Auto(auto);
+  await record.save();
+  user.createdAutos.push(record);
+  await user.save();
 }
 
 async function getById(id) {
-    const auto = Auto.findOne({ _id: id }).populate('owner').lean();
-    return auto;
+  const auto = Auto.findOne({ _id: id }).populate("owner").lean();
+  return auto;
 }
 
 async function edit(id, data) {
-    let record = await Auto.findByIdAndUpdate({ _id: id }, data);
-    return record.save();
+  let car = await Auto.findOne({ _id: id });
+  const images = data.imgUrl;
+  delete data.imgUrl;
+  console.log(car)
+  if(images) {
+      for (const img of images) {
+          car.imgUrl.push(img);
+      }
+      await car.save();
+  }
+  let record = await Auto.findByIdAndUpdate({ _id: id }, data);
+  return record.save();
 }
 
 async function deleteAuto(id, userId) {
-    console.log(id, userId)
-    const record = await User.findOneAndUpdate(
-        { _id: userId }, { "$pull": { "createdAutos": new mongoose.mongo.ObjectId(id) } },
-        { safe: true, multi: true, new: true });
-    await record.save();
-    Auto.findByIdAndRemove({ _id: id }, (err, docs) => {
-        try {
-            if (err) {
-                throw new Error(err);
-            } else {
-                console.log("Successful deletion " + docs);
-            }
-        } catch (err) {
-            console.log('Wrong data!');
-        }
-    });
+  const record = await User.findOneAndUpdate(
+    { _id: userId },
+    { $pull: { createdAutos: new mongoose.mongo.ObjectId(id) } },
+    { safe: true, multi: true, new: true }
+  );
+  await record.save();
+  Auto.findByIdAndRemove({ _id: id }, (err, docs) => {
+    try {
+      if (err) {
+        throw new Error(err);
+      } else {
+        console.log("Successful deletion " + docs);
+      }
+    } catch (err) {
+      console.log("Wrong data!");
+    }
+  });
 }
 
 module.exports = {
-    create,
-    getAll,
-    getById,
-    edit,
-    deleteAuto,
-    getCarsByCriteria,
-    getRecent
-}
+  create,
+  getAll,
+  getById,
+  edit,
+  deleteAuto,
+  getCarsByCriteria,
+  getRecent,
+};
