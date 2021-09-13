2334ab39c79e7c767cb904928fe512349ce316ec
Georgi Stoilkov <gogost6@gmail.com>
2021-09-13 21:32:00 +0300

    update

--- jdm/index.js
+++ jdm/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const databaseConfig = require("./server/config/database");
 const routesConfig = require("./server/config/routes");
 const expressConfig = require("./server/config/express");
 const storage = require("./server/middlewares/storage");
+
+const allowed = [
+  ".js",
+  ".css",
+  ".png",
+  ".jpg"
+];
 
 start();
 
@@ -17,7 +25,11 @@ async function start() {
   expressConfig(app);
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "jdm/src/index.html"));
+    if (allowed.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+        res.sendFile(path.resolve(`dist/jdm/${req.url}`));
+    } else {
+        res.sendFile(path.join(__dirname, "dist/jdm/index.html"));
+    }
 });
 
   app.listen(port, () =>
