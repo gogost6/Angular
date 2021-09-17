713cebde86704504107fb66f0a1783f9df4370bd
Georgi Stoilkov <gogost6@gmail.com>
2021-09-17 13:25:37 +0300

    update

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node ./jdm/index.js
--- jdm/index.js
+++ jdm/index.js
@@ -16,7 +16,7 @@ const allowed = [
 start();
 
 async function start() {
-  const port = 3000;
+  const port = process.env.PORT || 3000;
   const app = express();
 
   await databaseConfig(app);
