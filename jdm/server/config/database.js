ad8a6854fe6ccd5b9aae767f3c1b25076694f615
Georgi Stoilkov <gogost6@gmail.com>
2021-09-17 18:48:42 +0300

    db update

--- jdm/server/config/database.js
+++ jdm/server/config/database.js
@@ -2,17 +2,28 @@ const mongoose = require("mongoose");
 const config = require(".");
 
 module.exports = (app) => {
-  mongoose.connect(config.DB_CONNECTION, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  mongoose.set("useFindAndModify", false);
-
-  const db = mongoose.connection;
-  db.on("error", (err) => {
-    console.error("DB error: " + err);
-  });
-  db.once("open", function () {
-    console.log("DB connected!");
-  });
+  if(process.env.NODE_ENV) {
+    const { MongoClient } = require('mongodb');
+    const uri = "mongodb+srv://gogog:11111@cluster0.xfvou.mongodb.net/jdm?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+      const collection = client.db("test").collection("devices");
+      // perform actions on the collection object
+      client.close();
+    });
+  } else {
+    mongoose.connect(config.DB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    mongoose.set("useFindAndModify", false);
+  
+    const db = mongoose.connection;
+    db.on("error", (err) => {
+      console.error("DB error: " + err);
+    });
+    db.once("open", function () {
+      console.log("DB connected!");
+    });
+  }
 };
